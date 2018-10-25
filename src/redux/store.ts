2e88688b5e179eb9707693feb4f9ad39629fc634
Carita Logrén <carita.logren@gmail.com>
2018-10-26 00:41:19 +0300

    Fix: Use package for store + middlewares setup to avoid Android 'window not found' error.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,26 +1,19 @@
 // tslint:disable no-string-literal
 
-import { createStore, applyMiddleware, compose, Middleware } from 'redux';
+import { createStore, applyMiddleware, Middleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import { rootReducer } from './reducers';
 
-interface Window {
-  [key: string]: any; // Add index signature
-}
-
-const middleware = (additionalMiddleware: Middleware[] = []) =>
-  (window as Window)['__REDUX_DEVTOOLS_EXTENSION__']
-    ? [
-        applyMiddleware(thunk, ...additionalMiddleware),
-        (window as Window)['__REDUX_DEVTOOLS_EXTENSION__'](),
-      ]
-    : [applyMiddleware(thunk, ...additionalMiddleware)];
-
-export default function configureStore(additionalMiddleware: Middleware[]) {
-  const store = createStore(
+export default function configureStore(
+  additionalMiddleware: Middleware[] = []
+) {
+  return createStore(
     rootReducer,
-    compose(...middleware(additionalMiddleware))
+    composeWithDevTools(
+      applyMiddleware(thunk, ...additionalMiddleware)
+      // other store enhancers if any
+    )
   );
-  return store;
 }
