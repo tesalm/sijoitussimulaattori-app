ebfb02fd3e046ee897e73c8d4751f7824aa079c5
hheidiv <heidi.vulli@hotmail.com>
2018-11-18 22:09:29 +0200

    Started to implement the logic of the view

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,13 +1,16 @@
 import { Auth, authReducer } from './../Auth/reducer';
 import { combineReducers } from 'redux';
 import { counterReducer, Counter } from '../HelloFeature/reducer';
+import { SingleStock, stockReducer } from '../StockFeature/reducer';
 
 export interface RootState {
   counter: Counter;
   login: Auth;
+  singleStock: SingleStock;
 }
 
 export const rootReducer = combineReducers<RootState>({
   counter: counterReducer,
   login: authReducer,
+  singleStock: stockReducer
 });
