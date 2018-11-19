ef5a1213798cfe79f66287781e33705edcf3cc6a
hheidiv <heidi.vulli@hotmail.com>
2018-11-19 23:04:33 +0200

    Started using model for stock. Also got the network to work.

--- src/StockFeature/reducer.ts
+++ src/StockFeature/reducer.ts
@@ -1,20 +1,22 @@
 import { StockAction, ActionType } from './actions';
+import { Stock } from '../models/index';
 
 export interface SingleStock {
-  stock: Array<{
-    key: string,
-    bid: number,
-    offer: number,
-    high: number,
-    low: number,
-    marketValue: number,
-    revenue: number}>;
+  stock: Stock;
   loading: boolean;
   error: Error | null;
 }
 
 const initialState: SingleStock = {
-  stock: [],
+  stock: {
+    key: "",
+    bid: 0,
+    offer: 0,
+    high: 0,
+    low: 0,
+    marketValue: 0,
+    revenue: 0
+  },
   loading: false,
   error: null,
 };
@@ -24,7 +26,15 @@ export const stockReducer = (state: SingleStock = initialState, action: StockAct
     case ActionType.GetStockBegin:
       return {
         ...state,
-        stock: [],
+        stock: {
+          key: "",
+          bid: 0,
+          offer: 0,
+          high: 0,
+          low: 0,
+          marketValue: 0,
+          revenue: 0
+        },
         loading: true,
         error: null
       };
@@ -32,7 +42,15 @@ export const stockReducer = (state: SingleStock = initialState, action: StockAct
     case ActionType.GetStockSuccess:
       return {
         ...state,
-        stock: action.stock,
+        stock: {
+          key: action.stock.key,
+          bid: action.stock.bid,
+          offer: action.stock.offer,
+          high: action.stock.high,
+          low: action.stock.low,
+          marketValue: action.stock.marketValue,
+          revenue: action.stock.revenue
+        },
         loading: false,
         error: null
       };
@@ -40,7 +58,15 @@ export const stockReducer = (state: SingleStock = initialState, action: StockAct
     case ActionType.GetStockFailure:
       return {
         ...state,
-        stock: [],
+        stock: {
+          key: "",
+          bid: 0,
+          offer: 0,
+          high: 0,
+          low: 0,
+          marketValue: 0,
+          revenue: 0
+        },
         loading: false,
         error: action.error
       };
