1acf4a327ef380fbee537519dc63de367c02be11
hheidiv <heidi.vulli@hotmail.com>
2018-12-01 22:16:13 +0200

    Poistettu mergestä jäänyt tuplarivi.

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import { counterReducer, Counter } from '../HelloFeature/reducer';
 import { SingleStock, stockReducer } from '../Stock/reducer';
-
-import { Counter, counterReducer } from '../HelloFeature/reducer';
 import { StocksListing, stocksListingReducer } from '../MarketScreen/reducers';
 import { Auth, authReducer } from './../Auth/reducer';
 
