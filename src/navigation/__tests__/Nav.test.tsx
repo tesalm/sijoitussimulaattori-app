8f4b793b7fa622f3e13c594494c15961775e60fb
Teemu <teemu.2.salminen@student.tut.fi>
2018-12-02 20:40:16 +0200

    modifying navigation tests

--- src/navigation/__tests__/Nav.test.tsx
+++ src/navigation/__tests__/Nav.test.tsx
@@ -1,47 +1,37 @@
 import { mount, shallow } from 'enzyme';
 import * as React from 'react';
-import { NavigationActions } from 'react-navigation';
+import { NavigationActions, DrawerActions } from 'react-navigation';
 
-import MenuIcon from '../components/MenuIcon';
 import MainNavigator from '../MainNavigator';
 import { RouteName } from '../routes';
-
 import 'react-native';
-import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 
 describe('navigation', () => {
-  const wrapper = mount(<MainNavigator />);
+  const wrapper = shallow(<MainNavigator />);
   const navigator: any = wrapper.instance();
+  
+  it('routes', async () => {
+    const routes = Object.keys(RouteName);
+    routes.map((route) => {
+      expect(
+        navigator.dispatch(NavigationActions.navigate({ routeName: route }))
+      ).toEqual(true);
+    });
+  });
 
   it('bottom tabs', async () => {
     expect(navigator.state.nav.routes[0].routes.length).toBe(4);
 
-    wrapper
-      .find('[testID="Home"]')
-      .find(TouchableWithoutFeedback)
-      .props().onPress!({} as any);
-
+    navigator.dispatch(NavigationActions.navigate({routeName: RouteName.Home}));
     expect(navigator.state.nav.routes[0]).toMatchObject({ index: 0 });
 
-    wrapper
-      .find('[testID="Market"]')
-      .find(TouchableWithoutFeedback)
-      .props().onPress!({} as any);
-
+    navigator.dispatch(NavigationActions.navigate({routeName: RouteName.Market}));
     expect(navigator.state.nav.routes[0]).toMatchObject({ index: 1 });
 
-    wrapper
-      .find('[testID="Commissions"]')
-      .find(TouchableWithoutFeedback)
-      .props().onPress!({} as any);
-
+    navigator.dispatch(NavigationActions.navigate({routeName: RouteName.Commissions}));
     expect(navigator.state.nav.routes[0]).toMatchObject({ index: 2 });
 
-    wrapper
-      .find('[testID="Community"]')
-      .find(TouchableWithoutFeedback)
-      .props().onPress!({} as any);
-
+    navigator.dispatch(NavigationActions.navigate({routeName: RouteName.Community}));
     expect(navigator.state.nav.routes[0]).toMatchObject({ index: 3 });
 
     navigator.dispatch(NavigationActions.back());
@@ -49,13 +39,7 @@ describe('navigation', () => {
   });
 
   it('drawer toggle', async () => {
-    const menu = wrapper
-      .find(MenuIcon)
-      .find(TouchableOpacity)
-      .props();
-    //expect(menu).toMatchSnapshot();
-    menu.onPress!({} as any);
+    navigator.dispatch(DrawerActions.toggleDrawer());
     expect(navigator.state.nav.toggleId).toBe(1);
   });
 });
-
