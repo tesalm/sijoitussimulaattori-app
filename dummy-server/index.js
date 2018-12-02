b2dd10621966d8699ed66586e0a20c02c0365e0f
hheidiv <heidi.vulli@hotmail.com>
2018-12-02 20:50:19 +0200

    Combined two Stock-interfaces.

--- dummy-server/index.js
+++ dummy-server/index.js
@@ -5,17 +5,17 @@ const port = 3000;
 const fs = require('fs');
 
 const availableStocks = JSON.parse(fs.readFileSync(__dirname + '/data/availableStocks.json'));
-const singleStock = (key) =>
-  JSON.parse(fs.readFileSync(__dirname + '/data/' + key + '.json'));
+const singleStock = (symbol) =>
+  JSON.parse(fs.readFileSync(__dirname + '/data/' + symbol + '.json'));
 
 app.get('/stocks/list', (req, res) => {
   console.log("Connected...");
   res.json(availableStocks);
 });
 
-app.get('/stocks/list/:key', (req, res) => {
-  console.log(req.params.key + '-stock being fethced');
-  res.json(singleStock(req.params.key));
+app.get('/stocks/list/:symbol', (req, res) => {
+  console.log(req.params.symbol + '-stock being fethced');
+  res.json(singleStock(req.params.symbol));
 });
 
 app.listen(port, () => {  console.log(`Dummy server listening on http://localhost:${port}`);
