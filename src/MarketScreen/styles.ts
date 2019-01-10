e2536bbc6fbd0073409f644687743f05878e26b8
hheidiv <heidi.vulli@hotmail.com>
2019-01-10 20:27:11 +0200

    Added general styles to marketscreen.

--- src/MarketScreen/styles.ts
+++ src/MarketScreen/styles.ts
@@ -7,20 +7,11 @@ const StockStyles = StyleSheet.create({
   titleStyle: {
     fontWeight: 'bold',
     color: Colors.baseColor,
-    fontSize: verticalScale(16),
-    marginTop: verticalScale(8),
-    marginLeft: scale(8),
+    fontSize: verticalScale(17),
+    marginBottom: verticalScale(8),
   },
   subtitleView: {
     flexDirection: 'column',
-    marginTop: verticalScale(12),
-    marginLeft: scale(8),
-  },
-  lastSaleText: {
-    fontSize: verticalScale(12),
-    color: Colors.baseColor,
-    textAlign: 'justify',
-    marginBottom: verticalScale(0),
   },
   lastSaleValue: {
     fontSize: verticalScale(14),
@@ -31,25 +22,25 @@ const StockStyles = StyleSheet.create({
   },
   rightTitleView: {
     flexDirection: 'column',
-    marginLeft: scale(8),
-  },
-  revenueText: {
-    fontSize: verticalScale(12),
-    color: Colors.baseColor,
+    marginLeft: scale(16),
   },
   loadingView: {
     flex: 1,
     justifyContent: 'center',
   },
   whiteContainer: {
-    height: verticalScale(75),
+    flex: 1,
     backgroundColor: Colors.whiteBackground,
     borderBottomWidth: 0,
+    paddingLeft: scale(16),
+    paddingVertical: verticalScale(8),
   },
   greyContainer: {
-    height: verticalScale(75),
+    flex: 1,
     backgroundColor: Colors.greyBackground,
     borderBottomWidth: 0,
+    paddingLeft: scale(16),
+    paddingVertical: verticalScale(8),
   },
 });
 
