cf9ffea69c2c45f2485e84ced7dc09683e0622db
Carita Logrén <carita.logren@gmail.com>
2019-01-12 22:19:38 +0200

    Remove old hello world example component

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,19 +1,16 @@
 import { combineReducers } from 'redux';
 
-import { Counter, counterReducer } from '../HelloFeature/reducer';
 import { StocksListing, stocksListingReducer } from '../MarketScreen/reducer';
 import { User, userReducer } from '../User/reducer';
 import { Auth, authReducer } from './../Auth/reducer';
 
 export interface RootState {
-  counter: Counter;
   login: Auth;
   stocksListing: StocksListing;
   user: User;
 }
 
 export const rootReducer = combineReducers<RootState>({
-  counter: counterReducer,
   login: authReducer,
   stocksListing: stocksListingReducer,
   user: userReducer,
